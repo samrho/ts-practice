1fd4ec4915d56ab13f4ce829cb768277f502a4bb
Sam Rho <seungnam2@gmail.com>
2020-01-01 17:51:01 +0900

    example: 배열 내장 함수 filter 실습

--- src/examples/filter.ts
+++ src/examples/filter.ts
@@ -7,16 +7,16 @@ let users = [
 	{ id: 6, name: "JM", age: 32 },
 	{ id: 7, name: "HI", age: 24 },
 ];
-// (1)
+
 let temp_users = [];
 for (let i = 0, len = users.length; i < len; i++) {
 	if (users[i].age < 30) temp_users.push(users[i]);
 }
-console.log(temp_users.length);
-// 4
-// (2)
+
 let ages = [];
 for (let i = 0, len = temp_users.length; i < len; i++) {
 	ages.push(temp_users[i].age);
 }
-console.log(ages);
+
+// using built-in filter function of array
+const users_under30 = users.filter((user) => user.age < 30);
